9da09c4ce0d0570424aec35064d6f7d4ed83e27c
Felipe Aguiar Liota <Felipe@DESKTOP-BVMEUJI.SUPERINTEROP.COM.BR>
2018-07-02 08:45:52 -0300

    teste add php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!CODETYPE html>
 <html>
   <head>
+    <title>Minha página de teste </title>
   </head>
     <body>
     </p>
       <?php
           echo "Minha primeira linha em PHP!";
-      ?>
+          $myname = "Felipe";
+        ?>
       </p>
     </body>
 </html>
